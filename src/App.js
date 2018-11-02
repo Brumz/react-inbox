3658f499a2152cc99445b1e9e622d6b5ea6f8e66
Brumz <bmanrumsey24@gmail.com>
2018-11-02 10:57:21 -0600

    messageList and single message working

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
 import "./App.css";
 import Toolbar from "./components/Toolbar";
+import MessageList from "./components/MessageList";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Toolbar />
+        <MessageList />
       </div>
     );
   }
