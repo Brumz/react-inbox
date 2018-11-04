408e06f3164bafed533aabfa970d3f2411e9c2f4
Brumz <bmanrumsey24@gmail.com>
2018-11-04 08:45:53 -0700

    patch request to update message to read worjing in messageRead function

--- src/App.js
+++ src/App.js
@@ -19,8 +19,24 @@ class App extends Component {
     });
   }
 
-  messageRead = id => {
+  messageRead = async id => {
     console.log("messageRead", id);
+
+    let message = {
+      messageIds: [id],
+      command: "read",
+      read: true
+    };
+
+    const updateMessages = await fetch("http://localhost:8082/api/messages", {
+      method: "PATCH",
+      body: JSON.stringify(message),
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json"
+      }
+    });
+
     const updatedMessages = this.state.messages.map(message => {
       if (message.id === id) {
         message.read = !message.read;
